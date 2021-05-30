7f44ce2f2df0cf2b99f487e707c2bf095258c3db
Mike-Monta <83614687+Mike-Monta@users.noreply.github.com>
2021-05-30 21:54:00 +0200

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     half = 1
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
-    game.over(false)
-})
 info.onCountdownEnd(function () {
     game.over(false)
 })
@@ -21,12 +18,15 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`finish`, function (sprite, lo
         half = 0
     }
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
+    game.over(false)
+})
 let tiempo = 0
 let vueltas = 0
 let half = 0
 scene.setBackgroundColor(1)
 tiles.setTilemap(tilemap`track1`)
-scene.setBackgroundImage(assets.image`Menu`)
+scene.setBackgroundImage(assets.image`Fire 2`)
 let turbo = sprites.create(img`
     f f f f f f f f f f f f f f f f 
     f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
@@ -49,11 +49,11 @@ tiles.placeOnTile(turbo, tiles.getTileLocation(16, 29))
 half = 1
 let car1 = sprites.create(assets.image`car0`, SpriteKind.Player)
 let humo = sprites.create(assets.image`humo1`, SpriteKind.Player)
-humo.setImage(assets.image`vacio`)
 scene.cameraFollowSprite(car1)
 tiles.placeOnTile(car1, tiles.getTileLocation(7, 32))
 tiles.placeOnTile(humo, tiles.getTileLocation(7, 32))
 controller.moveSprite(car1, 100, 100)
+humo.setStayInScreen(false)
 game.onUpdate(function () {
     if (controller.right.isPressed()) {
         car1.setImage(assets.image`car1`)
@@ -68,9 +68,14 @@ game.onUpdate(function () {
         car1.setImage(assets.image`car3`)
     }
     if (controller.right.isPressed() || (controller.up.isPressed() || (controller.down.isPressed() || controller.left.isPressed()))) {
+        humo.setStayInScreen(true)
         humo.setImage(assets.image`humo1`)
-        humo.follow(car1, 98)
-    } else {
-    	
+        humo.follow(car1, 99)
     }
+})
+forever(function () {
+    scene.setBackgroundImage(assets.image`Fire 1`)
+    pause(500)
+    scene.setBackgroundImage(assets.image`Fire 2`)
+    pause(500)
 })
