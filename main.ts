28bb2443139bd42a51fb4258f5ea2b6ebce98bd0
IndomiusDan <84666610+IndomiusDan@users.noreply.github.com>
2021-06-06 13:28:24 +0200

    Añadido turbo y plátano .Para ir más rápido o más lento

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
+namespace SpriteKind {
+    export const power = SpriteKind.create()
+    export const slow = SpriteKind.create()
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     half = 1
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.slow, function (sprite, otherSprite) {
+    controller.player1.moveSprite(car1, 60, 60)
+    for (let index = 0; index < 5; index++) {
+        pause(1000)
+    }
+    controller.player1.moveSprite(car1, 100, 100)
 })
 info.onCountdownEnd(function () {
     game.over(false)
@@ -18,11 +29,19 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`finish`, function (sprite, lo
         half = 0
     }
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.power, function (sprite, otherSprite) {
+    controller.player1.moveSprite(car1, 125, 125)
+    for (let index = 0; index < 5; index++) {
+        pause(1000)
+    }
+    controller.player1.moveSprite(car1, 100, 100)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
     game.over(false)
 })
 let tiempo = 0
 let vueltas = 0
+let car1: Sprite = null
 let half = 0
 scene.setBackgroundColor(1)
 tiles.setTilemap(tilemap`track1`)
@@ -44,11 +63,13 @@ let turbo = sprites.create(img`
     f 2 2 2 2 2 8 8 8 8 2 2 2 2 2 f 
     f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
     f f f f f f f f f f f f f f f f 
-    `, SpriteKind.Player)
+    `, SpriteKind.power)
+let platano = sprites.create(assets.image`Platano`, SpriteKind.slow)
 tiles.placeOnTile(turbo, tiles.getTileLocation(16, 29))
+tiles.placeOnTile(platano, tiles.getTileLocation(23, 47))
 half = 1
-let car1 = sprites.create(assets.image`car0`, SpriteKind.Player)
-let humo = sprites.create(assets.image`humo1`, SpriteKind.Player)
+car1 = sprites.create(assets.image`car0`, SpriteKind.Player)
+let humo = sprites.create(assets.image`humo1`, SpriteKind.Food)
 scene.cameraFollowSprite(car1)
 tiles.placeOnTile(car1, tiles.getTileLocation(7, 32))
 tiles.placeOnTile(humo, tiles.getTileLocation(7, 32))
